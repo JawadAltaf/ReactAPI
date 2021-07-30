814d3145d2657007a16abc015f4130a8b181fcd1
JawadAltaf <jawaaltaf305@gmail.com>
2021-07-30 17:13:09 +0500

    Get API modified

--- src/GetAPI.jsx
+++ src/GetAPI.jsx
@@ -6,18 +6,23 @@ function GetAPI()
     const [data,setData] = useState([]);
     const [name,setName] = useState("");
     const [member_id, setMemberId] = useState("");
+    const [userId, setUserId] = useState(null);
     useEffect(()=>{
         getList();
     },[])
+
     function getList()
     {
         fetch("http://127.0.0.1:8000/api/list").then((result)=>{
-            result.json().then((resp)=>{
-                console.log("result", resp);
+            result.json().then((resp) => {
                 setData(resp);
+                setName(resp[0].name);
+                setMemberId(resp[0].member_id);
+                setUserId(resp[0].id);
             })  
         })
     }
+
     function saveUser()
     {
         let userData = {name,member_id};
@@ -48,6 +53,32 @@ function GetAPI()
         })
     }
 
+    function updateUser(uid)
+    {
+        console.log(data[uid-2]);
+        setName(data[uid].name);
+        setMemberId(data[uid].member_id);
+        setUserId(data[uid].id);
+    }
+    
+    function update()
+    {
+        let item = {name,member_id,userId};
+        fetch(`http://127.0.0.1:8000/api/update/${userId}`,{
+            method:"PUT",
+            headers:{
+                'Accept':'application/json',
+                'Content-Type':'application/json'
+            },
+            body:JSON.stringify(item)
+        }).then((result) => {
+            result.json().then((res) =>{
+                console.log(res);
+                getList();
+            })
+        })
+    }
+
     return(
 
         <Fragment>
@@ -68,17 +99,22 @@ function GetAPI()
                                 <td>{values.id}</td>
                                 <td>{values.name}</td>
                                 <td>{values.member_id}</td>
-                                <td><Button variant="danger" onClick={() => deleteUser(values.id)}>Delete</Button></td>
+                                <td>
+                                    <Button variant="danger" onClick={() => deleteUser(values.id)}>Delete</Button>&nbsp;
+                                    <Button variant="info" onClick={() => updateUser(values.id)}>Update</Button>
+                                </td>
                             </tr>
                         ))
                     }
                 </tbody>
-            </Table> <br/><br/>
+            </Table> 
+            <br/><br/>
             <div style={{ width:"100%", textAlign:"center" }}>
                 <form>
                     <input type="text" value={name} name="name" onChange={(e)=>{setName(e.target.value)}}/>&nbsp;
                     <input type="number" value={member_id} name="member_id" onChange={(e)=>{setMemberId(e.target.value)}}/>&nbsp;
-                    <Button type="button" variant="success" onClick={saveUser}>Save new user</Button>
+                    <Button type="button" variant="success" onClick={saveUser}>Save new user</Button>&nbsp;
+                    <Button type="button" variant="primary" onClick={update}>Update new user</Button>
                 </form>
             </div>
         </Fragment>
